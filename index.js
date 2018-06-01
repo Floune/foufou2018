631a3059fd59d3de4dadf0bea5bdb0aafd49abed
floune <robin.sangla@live.fr>
2018-06-01 16:45:13 +0200

    procfilr

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 var path = require('path')
+const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 
-app.listen(3000, function () {
- 
-})
+app.listen(port, () => console.log(`listening on port ${port}`));
