c734c9042590e2fca14bd86416bedd960218e4a5
floune <robin.sangla@live.fr>
2018-06-05 19:46:01 +0200

    credits

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 var path = require('path')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'public/index.html'));
+   res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/ontaimepapa', function(req, res) {
+   res.sendFile(path.join(__dirname + '/public/credits.html'));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
